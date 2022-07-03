25ce8e5800d0fab3753ca9078db4a5ff5e20d43c
Samson Akanbi <samson.akanbi@outlook.com>
2022-07-03 23:48:53 +0100

    Confirmed if the account has enough money before sending.

--- src/main/java/com/example/nibss/repository/AccountRepository.java
+++ src/main/java/com/example/nibss/repository/AccountRepository.java
@@ -2,8 +2,14 @@ package com.example.nibss.repository;
 
 import com.example.nibss.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    @Query("SELECT a FROM Account a WHERE a.accountNumber = ?1")
+    Optional<Account> findByTheAccountNumber(String accountNumber);
 }
--- src/main/java/com/example/nibss/service/TransferService.java
+++ src/main/java/com/example/nibss/service/TransferService.java
@@ -1,19 +1,25 @@
 package com.example.nibss.service;
 
+import com.example.nibss.model.Account;
 import com.example.nibss.model.Transfer;
+import com.example.nibss.repository.AccountRepository;
 import com.example.nibss.repository.TransferRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TransferService {
     private final TransferRepository transferRepository;
 
+    private final AccountRepository accountRepository;
+
     @Autowired
-    public TransferService(TransferRepository transferRepository) {
+    public TransferService(TransferRepository transferRepository, AccountRepository accountRepository) {
         this.transferRepository = transferRepository;
+        this.accountRepository = accountRepository;
     }
 
     public List<Transfer> getTransfers() {
@@ -21,6 +27,26 @@ public class TransferService {
     }
 
     public Transfer newTransfer(Transfer transfer) {
+         // Get destination account by account number
+        Optional<Account> destinationAccount = accountRepository
+                .findByTheAccountNumber(transfer.getDestinationAccount());
+
+        Optional<Account> sourceAccount = accountRepository
+                .findByTheAccountNumber(transfer.getSourceAccount());
+
+        if (destinationAccount.isEmpty()) {
+            throw new IllegalStateException("destination account doesn't exist");
+        }
+
+        if (sourceAccount.isPresent()) {
+            // Check if the source account has enough money before saving
+            if (transfer.getAmount() > sourceAccount.get().getBalance()) {
+                throw new IllegalStateException("Insufficient funds");
+            } else {
+                System.out.println("Sufficient funds");
+            }
+        }
+
         return transferRepository.save(transfer);
     }
 }
